d44a98d7374ce1bfe0615e854b3943b997ae2b63
Gavin Wiggins <6828967+wigging@users.noreply.github.com>
2023-12-10 16:41:55 -0500

    Add code for day 9

---
+++ day09.py
@@ -0,0 +1,81 @@
+"""
+Day 9 for Advent of Code 2023.
+https://adventofcode.com/2023/day/9
+"""
+
+
+def get_line(text_file):
+    """
+    Yield a line from the text file.
+    """
+    with open(text_file) as file:
+        for line in file:
+            yield line.rstrip()
+
+
+def run_part1(text_file):
+    """
+    Run the example or entire problem for part 1.
+    """
+    value = 0
+
+    for line in get_line(text_file):
+        hist = [int(x) for x in line.split()]
+
+        hists = []
+        hists.append(hist)
+
+        while True:
+            diff = [hists[-1][i+1] - hists[-1][i] for i in range(len(hists[-1]) - 1)]
+            hists.append(diff)
+            if all(v == 0 for v in hists[-1]):
+                break
+
+        for h in hists[::-1]:
+            value += h[-1]
+
+    print(f'Part 1 answer -> {value}')
+
+
+def run_part2(text_file):
+    """
+    Run the example or entire problem for part 2.
+    """
+    values = []
+
+    for line in get_line(text_file):
+        hist = [int(x) for x in line.split()]
+
+        hists = []
+        hists.append(hist)
+
+        while True:
+            diff = [hists[-1][i+1] - hists[-1][i] for i in range(len(hists[-1]) - 1)]
+            hists.append(diff)
+            if all(v == 0 for v in hists[-1]):
+                break
+
+        # print('\n', hists)
+
+        val = 0
+        vals = []
+        for h in hists[::-1]:
+            # print(h)
+            val = h[0] - val
+            # print(val)
+            vals.append(val)
+
+        values.append(vals[-1])
+
+    # print('\n', values)
+    print(f'Part 2 answer -> {sum(values)}')
+
+
+def main():
+    puzzle_input = "input/day09.txt"
+    run_part1(puzzle_input)
+    run_part2(puzzle_input)
+
+
+if __name__ == "__main__":
+    main()
